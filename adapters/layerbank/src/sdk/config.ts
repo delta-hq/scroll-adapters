731cd59351158b74cc0bf68ad84293e70b2bc39f
marsclimber0109 <marsclimber0109@gmail.com>
2024-06-19 22:55:46 +0900

    chore: url change

--- adapters/layerbank/src/sdk/config.ts
+++ adapters/layerbank/src/sdk/config.ts
@@ -9,14 +9,13 @@ export const enum PROTOCOLS {
 export const SUBGRAPH_URLS = {
   [CHAINS.SCROLL]: {
     [PROTOCOLS.LAYERBANK]: {
-      url: "https://api.goldsky.com/api/public/project_clwadadu5rddf01xa3m0m8ep0/subgraphs/layerbank_scroll/1.0.0/gn",
+      url: "https://api.goldsky.com/api/public/project_clwadadu5rddf01xa3m0m8ep0/subgraphs/layerbank-scroll/1.0.0/gn",
     },
   },
 };
 
 export const RPC_URLS = {
-  [CHAINS.SCROLL]:
-    "https://scroll-mainnet.blastapi.io/0f113c5d-4147-4d60-b20f-5915468a24c8",
+  [CHAINS.SCROLL]: "https://scroll.drpc.org",
 };
 
 export const WETH_ADDRESS = {
